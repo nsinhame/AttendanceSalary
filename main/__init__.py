72d0a4833def4daf85bfa115a0a27513a36ae2f1
Lavish <kumar.lavish.0109@gmail.com>
2023-08-17 18:33:20 +0530

    Added Login Page and Boss Login Credentials

--- main/__init__.py
+++ main/__init__.py
@@ -3,14 +3,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)                                                            # Initialize the app
-app.config["SECRET_KEY"] = "5791628bb0b13ce0c6f6dfde280ba245"                    # Set the secret key
+app.config["SECRET_KEY"] = "5691628bb0b13ce0c6f6dfde280ba245"                    # Set the secret key
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"                      # Initialize the database
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True                            # Set it False for production environment
 db = SQLAlchemy(app)                                                             # Initialize database variable
-bcrypt = Bcrypt(app)                                                             # Initialize bycryption variable
+bcrypt = Bcrypt(app)                                                         # Initialize bycryption variable
+login_manager = LoginManager(app)
+login_manager.login_view = "home"
+login_manager.login_message_category = "info"
+
+
 
 
 # Routes file is called here so that we can work with different funtions
--- main/routes.py
+++ main/routes.py
@@ -1,12 +1,33 @@
-from main import app, db
+from main import app, db, bcrypt
 from flask import render_template, redirect, url_for, flash, request
 from main.models import WorkerPrimary, WorkerTodayAttendance, WorkerAttendance, WorkerSalary, LocationData, SiteData, ProjectData, SiteEngineerDetails, SupervisorDetails, Boss
 from datetime import datetime, timedelta
 import calendar
+from flask_login import login_user, current_user, logout_user, login_required
 
 
-@app.route("/", methods=["GET", "POST"])
-@app.route("/layout", methods=["GET", "POST"])
-def home():
-    first_name_ = request.form.get("f_name")
-    return redirect(url_for("layout"))
+@app.route("/", methods=["GET", "POST"])                             # Route for the opening page
+@app.route("/homepage", methods=["GET", "POST"])                   # Route when someone tries to navigate to first page
+def front_page():
+    return render_template("homepage.html", title="Home Page")
+
+@app.route("/login", methods=["GET", "POST"])
+def LoginForm():
+    if current_user.is_authenticated:
+        return redirect(url_for("explore"))
+    if request.method == "POST":
+        _email = request.form.get("inputEmail")
+        _password = request.form.get("inputPassword")
+        # _user = User.query.filter_by(email=_email).first()
+        # if _user and bcrypt.check_password_hash(_user.password, _password):
+        #     login_user(_user, remember=_remember)
+        #     flash("Login Successful", "success")
+            # return redirect(url_for("profile"))
+        # Sample:
+        if _email == "Boss01@1" and _password == "123qwe":
+            flash("Login Successful", "info")
+            return render_template("bosslogin.html", title = "Boss Login")
+        else:
+            flash("Login Unsuccessful. Please check email and password", "danger")
+            return render_template("homepage.html", title="Home")
+    return redirect(url_for("homepage"))
