ee45523637291ba6499dd964a8f05646879a6c07
Lavish <kumar.lavish.0109@gmail.com>
2023-08-18 18:15:45 +0530

    Added background color, liked the login page and the boss page, added documentation

--- main/routes.py
+++ main/routes.py
@@ -9,25 +9,19 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/", methods=["GET", "POST"])                             # Route for the opening page
 @app.route("/homepage", methods=["GET", "POST"])                   # Route when someone tries to navigate to first page
 def front_page():
-    return render_template("homepage.html", title="Home Page")
+    return render_template("home.html", title="Home Page")
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/submit_login_credit", methods=["GET", "POST"])
 def LoginForm():
     if current_user.is_authenticated:
-        return redirect(url_for("explore"))
+        return redirect(url_for("bosslogin"))
     if request.method == "POST":
-        _email = request.form.get("inputEmail")
-        _password = request.form.get("inputPassword")
-        # _user = User.query.filter_by(email=_email).first()
-        # if _user and bcrypt.check_password_hash(_user.password, _password):
-        #     login_user(_user, remember=_remember)
-        #     flash("Login Successful", "success")
-            # return redirect(url_for("profile"))
-        # Sample:
-        if _email == "Boss01@1" and _password == "123qwe":
-            flash("Login Successful", "info")
+        input_id = request.form["input_id"]
+        input_password = request.form["input_password"]
+        if input_id == "Boss01@1" and input_password == "123qwe":
+            flash("Login Successful", "success")
             return render_template("bosslogin.html", title = "Boss Login")
         else:
-            flash("Login Unsuccessful. Please check email and password", "danger")
-            return render_template("homepage.html", title="Home")
-    return redirect(url_for("homepage"))
+            flash("Login Unsuccessful. Please check email and password", "warning")
+            return render_template("home.html", title="Home")
+    return render_template("bosslogin.html", title = "Boss Login")
